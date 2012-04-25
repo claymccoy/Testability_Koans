c23062d247e39dd74d0c05a7d1489591e05b4fc1
Clay McCoy <git@claymccoy.com>
2012-04-25 14:11:33 -0700

    solution: inject collaborator dependency

--- src/main/java/ClassUnderTest.java
+++ src/main/java/ClassUnderTest.java
@@ -4,8 +4,8 @@ public class ClassUnderTest {
 
     final Collaborator collaborator;
 
-    public ClassUnderTest() {
-        this.collaborator = new Collaborator();
+    public ClassUnderTest(Collaborator collaborator) {
+        this.collaborator = collaborator;
     }
 
     String sayHello() {
--- src/test/groovy/HiddenDependency1.groovy
+++ src/test/groovy/HiddenDependency1.groovy
@@ -2,7 +2,12 @@ import spock.lang.*
 
 class HiddenDependency1 extends Specification {
 
-    final classUnderTest = new ClassUnderTest()
+    final collaborator = new Collaborator() {
+        String collaborate() {
+            "Hello World!"
+        }
+    }
+    final classUnderTest = new ClassUnderTest(collaborator)
 
     def "should say hello"() {
         expect:
